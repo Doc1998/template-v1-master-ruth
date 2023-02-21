26d860e08c2d6307e980f6d61d084cc5d484c97d
Doc1998 <docmp98@gmail.com>
2023-02-21 12:07:00 -0800

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ContactComponent } from "./contact/contact.component";
 import { LandingComponent } from "./landing/landing.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ReviewsComponent } from "./reviews/reviews.component";
+import { ShopComponent } from "./shop/shop.component";
 
 const routes: Routes = [
   { path: "home", component: LandingComponent , data: {title : 'Fine Art & Fashion Illustrations | RuthTeresaArt'}},
@@ -12,7 +13,9 @@ const routes: Routes = [
   { path: "about-us", component: AboutUsComponent , data: {title : 'About Us | Fine Art & Fashion Illustrations | RuthTeresaArt'}},
   { path: "reviews", component: ReviewsComponent , data: {title : 'Reviews | Fine Art & Fashion Illustrations | RuthTeresaArt'} },
   { path: "portfolio", component: ProjectsComponent , data: {title : 'Portfoilio | Fine Art & Fashion Illustrations | RuthTeresaArt'} },
-  { path: "contact", component: ContactComponent  , data: {title : 'Contact | Fine Art & Fashion Illustrations | RuthTeresaArt'}}
+  { path: "contact", component: ContactComponent  , data: {title : 'Contact | Fine Art & Fashion Illustrations | RuthTeresaArt'}},
+  { path: "shop", component: ShopComponent  , data: {title : 'Contact | Fine Art & Fashion Illustrations | RuthTeresaArt'}}
+
 ];
 
 @NgModule({
